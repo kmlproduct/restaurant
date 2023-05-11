86a2a013ef4e26692315a71668fd1e855fd1bac3
kmlproduct <kam.salaheddine@gmail.com>
2023-05-11 21:13:05 +0100

    add our-app section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 document.getElementById('hamberger').onclick = function () {
     document.getElementById('menu').classList.toggle('show-menu')
 }
+const categorys = document.querySelectorAll('.category')
+for (var i = 0; i < categorys.length; i++) {
+    categorys[i].onmouseover = function () {
+        this.classList.toggle('active')
+    }
+    categorys[i].onmouseout = function () {
+        this.classList.remove('active')
+    }
+
+}
